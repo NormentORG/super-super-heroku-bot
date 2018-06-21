109d2e5d1d7afabda4b3415c3482861a3b9e1925
dhtdht020 <dhtdht020@gmail.com>
2018-06-21 16:57:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,5 +23,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'אז מי נגר?') {
+    	message.reply('כולם נגרים בדרך שונה');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
