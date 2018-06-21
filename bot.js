443e5e6d5ec642d6b593e933cac016681f5a9cc0
dhtdht020 <dhtdht020@gmail.com>
2018-06-21 19:29:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -297,5 +297,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'w.stop') {
+    	message.reply('נגר V');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
