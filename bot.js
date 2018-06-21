729f8a6457777075d21da439ea4256171014ea48
dhtdht020 <dhtdht020@gmail.com>
2018-06-21 19:28:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -291,5 +291,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'OOF') {
+    	message.reply('OOOOOOOOOOOOF');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
