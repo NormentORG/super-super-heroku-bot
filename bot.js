5e6e11375c0f2b628dafa8c403cde23ad04137e2
dhtdht020 <dhtdht020@gmail.com>
2018-06-21 19:31:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -303,5 +303,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'יואוו') {
+    	message.reply('יואוווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווווו');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
