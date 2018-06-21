ad6b68d0738aa6ae867e2aa4ab2186c459341faa
dhtdht020 <dhtdht020@gmail.com>
2018-06-21 17:15:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -103,5 +103,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'עזרה') {
+    	message.reply('אל תהיה נגר');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
