03a12efd879be941d639405385892a887911c33e
dhtdht020 <dhtdht020@gmail.com>
2018-06-21 17:35:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -212,5 +212,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'אילון נגר?') {
+    	message.reply('התשובה תיהיה כן בכי נגר');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
