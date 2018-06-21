142c96ef23e3afe8823aa687ff7c8fc3417a3983
dhtdht020 <dhtdht020@gmail.com>
2018-06-21 16:50:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-bot.on('ready', function() {
-    bot.user.setUsername("Forknut");
-}
-bot.login("token");
-
 client.on('ready', () => {
     console.log('I am ready!');
 });
