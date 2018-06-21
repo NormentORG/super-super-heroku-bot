9f4f108454d4af3295a8aaf5115074354667ee0d
dhtdht020 <dhtdht020@gmail.com>
2018-06-21 17:58:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -279,5 +279,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'אני לא נגר') {
+    	message.reply('אתה כן נגר');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
