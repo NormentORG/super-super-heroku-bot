bdccdcec35161821093767fa4c04e164a92a9fc7
dhtdht020 <dhtdht020@gmail.com>
2018-06-21 17:38:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -218,5 +218,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'הכי נגר') {
+    	message.reply('אילון נגר?');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
