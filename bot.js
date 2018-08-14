d4796bd89f3bf1be2ce133bd5c3984507564217d
dhtdht020 <dhtdht020@gmail.com>
2018-08-14 14:09:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -489,5 +489,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === message.content) {
+    	message.reply(message.content);
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
